ec8ebb342240786179d6009248f876130a20295b
behkamfallah <behkamf@gmail.com>
2024-04-23 10:21:27 +0200

    Update the 01 Project and add finishing touches

--- 01.Story_teller/1.Story_Teller.py
+++ 01.Story_teller/1.Story_Teller.py
@@ -1,15 +1,22 @@
-def get_input(word_type):
+def get_input(word_type: str):
     user_input = input(f"Enter a {word_type}: ")
+
+    # Check if user's input does not contain digits or other characters than Alphabet.
     while not user_input.isalpha():
-        print(type(user_input))
         print(f"Please enter a word ({word_type})!")
         user_input = input(f"Try again!: ")
-    return(user_input)
 
-noun1 = get_input('noun')
-verb1 = get_input('verb (Present tense)')
-noun2 = get_input('noun')
-verb2 = get_input('verb (Present tense)')
+    return user_input
+
+
+# Variables that contain user input and call the 'get_input' function.
+noun1: str = get_input('noun')
+noun1 = noun1.title()  # It capitalizes the first character of a string.
+verb1: str = get_input('verb (Present tense)')
+noun2: str = get_input('noun')
+noun2 = noun2.title()
+verb2: str = get_input('verb (Present tense)')
+
 
 story = f"""
 Once upon a time, there was a {noun1} who loved to {verb1} all day.
